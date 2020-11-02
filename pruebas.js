b3a93f8a88e13c27e49ab1a10a35c5171fde609a
Brayan Getial <b.brayan.getial@gmail.com>
2020-11-01 23:48:21 -0500

    se quita la funcionalidad de las teclas y se agrega el modo automatico con un boton de detener

--- pruebas.js
+++ pruebas.js
@@ -1,34 +1,39 @@
 const barra = document.getElementById("bar")
-document.addEventListener("keydown", dibujar)
+const btnDetener = document.getElementById("detener")
+btnDetener.addEventListener("click", detener)
+//document.addEventListener("keydown", dibujar)
 
 barra.style.height = '20px'
-barra.style.width = '50%'
+barra.style.width = '100%'
 barra.style.background = '#000'
 var ancho = parseInt(barra.style.width)
+var intervaloDisminuir
+var intervaloAumentar
 
-var teclas ={
-    UP:38,
-    DOWN:40,
-    LEFT:37,
-    RIGHT:39
-}
+// var teclas ={
+//     UP:38,
+//     DOWN:40,
+//     LEFT:37,
+//     RIGHT:39
+// }
 
-
-function dibujar(ev) {
-    if(ev.keyCode == teclas.LEFT)
-    {
-        disminuir()
-    }
-    if(ev.keyCode == teclas.RIGHT)
-    {
-        aumentar()
-    }
-}
+// function dibujar(ev) {
+//     if(ev.keyCode == teclas.LEFT)
+//     {
+//         disminuir()
+//     }
+//     if(ev.keyCode == teclas.RIGHT)
+//     {
+//         aumentar()
+//     }
+// }
 function disminuir(){
     if(ancho >= 1)
         {
             ancho--
         } else {
+            clearInterval(intervaloDisminuir)
+            intervaloAumentar = setInterval(aumentar, 100)
             ancho = 0;
         }
         mostrar(ancho)
@@ -38,6 +43,8 @@ function aumentar(){
         {
             ancho++
         } else {
+            clearInterval(intervaloAumentar)
+            intervaloDisminuir = setInterval(disminuir, 100)
             ancho = 100;
         }
         mostrar(ancho)
@@ -46,3 +53,9 @@ function aumentar(){
 function mostrar(ancho) {
     barra.style.width = `${ancho}%`
 }
+function detener(){
+    clearInterval(intervaloAumentar)
+    clearInterval(intervaloDisminuir)
+}
+
+intervaloDisminuir = setInterval(disminuir, 100)
